cfffddcbf2c8d2c331434b0b1c47f8d79c72a4f0
HieuHKhong <hieu.khong@mscoding.org>
2024-09-09 10:51:47 -0500

    Styled a little bit.

--- js/app.js
+++ js/app.js
@@ -23,14 +23,15 @@ inputDate.addEventListener("click", function verification(){
     console.log(birth)
     //Compares data to determine if valid
     let secretmessage = document.getElementById("Secret");
+    let gif1 = document.createElement("img");
+    gif1.src = "./img/ofage.gif"
 
     if (year - birthyear > 18){
         secretmessage.innerHTML = "You're an Adult!"
+        document.getElementById("message").appendChild("img")
     }else if (year - birthyear === 18 && month >= birthmonth && day >= daybirth){
         secretmessage.innerHTML = "You're an Adult!"
     }else
         secretmessage.innerHTML = "You're too Young!"
-
-        
         
 }) 
